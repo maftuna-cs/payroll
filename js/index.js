4ac1b60f1a1f8a15892a792f342f390ad60c85e8
mac <mac@macui-MacBookPro.local>
2019-12-10 02:10:28 +0900

    .calc-taxes

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ function calc() {
     let master = parseInt(document.getElementById("chooseFaculty").value);
     let bachelor = parseInt(document.getElementById("chooseRegular").value);
     let fixedSal = parseInt(document.getElementById("txtFixSal").value);
-    let empType;
+    let staff;
     let incomeTax;    
 
     if(empCode == 'f' || empCode == 'F') {
-    
+    staff = 'faculty';
         if(master.checked) {
             perHour = 175;
             allowance = 1500; //per month
@@ -23,7 +23,8 @@ function calc() {
         }
     
         grosSalary = hrs * perHour + allowance;
-
+        
+        //taxFee
         if (grosSalary > 3000) {
             incomeTax = (.25/grosSalary)*100;
         }
@@ -41,21 +42,41 @@ function calc() {
     netPay = (hrs * perHour + allowance) - (healthFee + incomeTax);
 
     if (empCode == 'R' || empCode == 'r' )
+    staff = 'regular';
     //enter working hour for 'regular' 
+
     if (hrs == 160) {
         grosSalary = fixedSal;
+        fixedSal = hrlyRate * 160;
         
     }
+    //proRate
     else if (hrs < 160) {
         hrlyRate = fixedSal/160;
-        proRate = hrlyRate * hrs;
+        grosSalary = hrlyRate * hrs;
     }
-    
+    //overRate
     else if (hrs > 160) {
-        overRate = (hrs - 160)*2;
+        grosSalary = (hrs - 160)*2;
     }
     
+    //taxFee
+    if (grosSalary > 3000) {
+        incomeTax = (.25/grosSalary)*100;
+    }
 
-    
+    else if (grosSalary = 2500) {
+        income_tax = 'tax free';
+    }
+
+    if (grosSalary > 3000) {
+        healthFee = 33;
+    }
+    else {
+        healthFee = 19.2;
 }
+netPay = grosSalary - (healthFee + incomeTax);       
 }
+
+}
+button.addEventListener('click',calc);
